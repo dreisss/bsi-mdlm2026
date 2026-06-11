4734dc5cf2acd0ec9ee3d5315d02c745e489b381
Davi Reis <dreisss.dev@gmail.com>
2026-06-11 20:46:38 -0300

    feat: add exam solutions

---
+++ final-work/linear-regression.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+# Tema: O Uso da Regressão Linear em Python como Ferramenta Didática para o Estudo de Fenômenos Físicos no Ensino Médio.
+# A aplicação ($x$ e $y$): Implementar o algoritmo utilizando dados de experimentos
+# reais (como a relação entre Massa ($x$) e Extensão de uma mola ($y$) — Lei de Hooke)
+# para mostrar como a programação ajuda a validar leis científicas.
+
+while True:
+    
